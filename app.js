26a5b26b80358bf48d573d659ff0589dbd33ba89
Sreeja48 <sreeja.sivasankar1920@gmail.com>
2024-10-14 12:26:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = 3000;
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'MySecureP@ss1',
+    password: 'Tinku@48',
     database: 'social_app'
 });
 
